9b7e97f6f2da366a69fe333bff4fb633109f2f76
Binshareefy <sharifabdulmajid601@gmail.com>
2026-06-12 20:43:01 +0100

    Fix: Force compileSdk=36 for all subprojects including plugins

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,16 @@ allprojects {
     }
 }
 
+subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            project.android {
+                compileSdk = 36
+            }
+        }
+    }
+}
+
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
